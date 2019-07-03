2b2bd680ee607f06014bb14ce9c43c8bc7ebd75d
zhangjing <zhangjing@huixiangtech.cn>
2019-07-03 18:31:58 +0800

    添加页面

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$('.header .menuBtn').click(function() {
+	$('.menubox').show().addClass('active');
+})
+$('.menubox .close,.menubox .mask').click(function(e) {
+	e.stopPropagation();
+	$('.menubox').removeClass('active');
+	setTimeout(function() {
+		$('.menubox').hide()
+	}, 500)
+});
+$('.menubox .hassublist').click(function() {
+	if ($(this).next('.sublist').is(':hidden')) {
+		$('.menubox .hassublist.active').removeClass('active');
+		$(this).addClass('active');
+		$('.sublist').slideUp();
+		$(this).next('.sublist').slideDown()
+	} else {
+		$(this).removeClass('active');
+		$(this).next('.sublist').slideUp()
+	}
+})
+
+$('.header .searchBtn').click(function(){
+	$('.searchbar').slideToggle('fast');
+})
